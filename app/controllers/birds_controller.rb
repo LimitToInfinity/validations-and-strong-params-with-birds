e9728dc3bce73d31a60eca91a8256301d5abc72d
LimitToInfinity <21stLimitless@gmail.com>
2020-10-28 11:44:06 -0600

    Adds validations and strong params for birds.

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,2 +1,27 @@
 class BirdsController < ApplicationController
+
+  def index
+    @birds = Bird.all
+    render json: @birds
+  end
+
+  def create
+    @bird = Bird.new(bird_params)
+
+    if @bird.valid?
+      @bird.save
+      render json: @bird, status: :created
+    else
+      render json:
+        { errors: @bird.errors.full_messages },
+        status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def bird_params
+    params.require(:bird).permit(:name, :color, :age)
+  end
+
 end
--- app/models/bird.rb
+++ app/models/bird.rb
@@ -1,2 +1,8 @@
 class Bird < ApplicationRecord
+
+  # validates(:name, { presence: true })
+  validates :name, :color, :age, presence: true
+  validates :name, uniqueness: true
+  validates :age, numericality: { greater_than: 0 }
+
 end
