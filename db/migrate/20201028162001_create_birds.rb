448accb1aecf0b42c11432da46b28e59889849fc
LimitToInfinity <21stLimitless@gmail.com>
2020-10-28 10:20:14 -0600

    Adds Bird resource with name, color, and age.

---
+++ db/migrate/20201028162001_create_birds.rb
@@ -0,0 +1,11 @@
+class CreateBirds < ActiveRecord::Migration[6.0]
+  def change
+    create_table :birds do |t|
+      t.string :name
+      t.string :color
+      t.integer :age
+
+      t.timestamps
+    end
+  end
+end
