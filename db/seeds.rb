7744b09b5497e8ca2788a6db4611a869ef4d5d03
LimitToInfinity <21stLimitless@gmail.com>
2020-10-28 10:22:20 -0600

    Adds 3 Bird seeds and migrates.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Bird.create(name: 'Squaky', color: 'blue', age: 3)
+Bird.create(name: 'Flyer', color: 'yellow', age: 6)
+Bird.create(name: 'Soar', color: 'red', age: 2)
